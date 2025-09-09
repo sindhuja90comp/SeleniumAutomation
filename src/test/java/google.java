44bcd6d3bc6a1c5a0d0c5d17a96bcc381761cda8
Sindhuja Martha <sindhuja90compscienceprograms@gmail.com>
2025-09-09 16:17:12 -0500

    update google.java

--- src/test/java/google.java
+++ src/test/java/google.java
@@ -6,8 +6,13 @@ public class google {
 
    public static void main(String[] args){
 
+       // Setup ChromeDriver automatically (no need to download manually)
        WebDriverManager.chromedriver().setup();
+
+       // Create a new instance of Chrome browser
        WebDriver driver = new ChromeDriver();
+
+       // Open Google homepage in the Chrome browser
        driver.get("https://www.google.com");
 
    }
