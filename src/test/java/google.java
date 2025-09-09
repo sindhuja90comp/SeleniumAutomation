5a24c36052ee629b581b9efc4454524f56639889
Sindhuja Martha <sindhuja90compscienceprograms@gmail.com>
2025-09-09 16:11:32 -0500

    add maven project

---
+++ src/test/java/google.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class google {
+
+   public static void main(String[] args){
+
+       WebDriverManager.chromedriver().setup();
+       WebDriver driver = new ChromeDriver();
+       driver.get("https://www.google.com");
+
+   }
+
+
+}
